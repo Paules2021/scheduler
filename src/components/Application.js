e2a7f0c86bab56206af9a66450e983e49414dd7c
Pouya Elishaee <p.elishaee@email.com>
2022-02-24 17:50:15 -0500

    use promise.all for hooks

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,74 +7,36 @@ import Appointment from "components/Appointment";
 import axios from "axios";
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
 
-
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
+    appointments: {}
   })
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
 
   // updates the day state with the new day
   const setDay = (day) => {
     setState({ ...state, day });
   };
 
-  // updates the days state with the new array
-  const setDays = (days) => {
-    setState({ ...state, days });
-  };
-
 
   // useEffect to get days from API
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then((response) => {
-        setDays(response.data);
-      })
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      });
   }, [])
 
-  const appointmentArray = appointments.map((appointment) => {
+  const appointmentArray = dailyAppointments.map((appointment) => {
     return <Appointment
       key={appointment.id}
       {...appointment}
